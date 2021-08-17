15f4de890114d2b5154b8f9d51e42c299fc3bff6
Steve Hamblett <steve.hamblett@linux.com>
2021-08-17 17:00:11 +0100

    Simple GPIO example

---
+++ bin/iot_voice_gpio.dart
@@ -0,0 +1,69 @@
+/*
+ * Package : iot_voice
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 17/08/2021
+ * Copyright :  S.Hamblett
+ */
+
+import 'dart:io';
+import 'package:mraa/mraa.dart';
+import 'iot_voice_gpio_config.dart';
+
+/// Simple GPIO example on the Raspberry pi for IOT Voice.
+int main() {
+  final mraa = Mraa.fromLib(mraaLibraryPath)
+    ..noJsonLoading = noJsonLoading
+    ..useGrovePi = useGrovePi
+    ..initialise();
+
+  // Version
+  final mraaVersion = mraa.common.version();
+  print('Mraa version is : $mraaVersion');
+
+  print('Initialising MRAA');
+  final ret = mraa.common.initialise();
+  if (ret != MraaReturnCode.success) {
+    print('Failed to initialise MRAA, return code is '
+        '${returnCode.asString(ret)}');
+  }
+
+  print('Getting platform name');
+  final platformName = mraa.common.platformName();
+  print('The platform name is : $platformName');
+
+  /// The LED initialisation
+  print('Initialising LED');
+  final ledContext = mraa.gpio.initialise(ledGPIOPin);
+
+  /// The switch initialisation
+  print('Initialising switch');
+  final switchContext = mraa.gpio.initialise(switchGPIOPin);
+
+  print('Flash the LED for 10 seconds');
+  // Note that on my board 0 turns the LED on, 1 Off
+  for (var i = 0; i <= 9; i++) {
+    mraa.gpio.write(ledContext, 0);
+    sleep(const Duration(milliseconds: 500));
+    mraa.gpio.write(ledContext, 1);
+    sleep(const Duration(milliseconds: 500));
+  }
+
+  print('Pressing the switch will now light the LED, press X to exit');
+  var stop = false;
+  while (!stop) {
+    var switchValue = mraa.gpio.read(switchContext);
+    if (switchValue == 1) {
+      mraa.gpio.write(ledContext, 0);
+    } else {
+      mraa.gpio.write(ledContext, 1);
+    }
+    sleep(const Duration(milliseconds: 50));
+    final char = String.fromCharCode(stdin.readByteSync());
+    if (char.toUpperCase() == 'X') {
+      stop = true;
+    }
+  }
+
+  print('GPIO test complete');
+  return 0;
+}
---
+++ bin/iot_voice_gpio_config.dart
@@ -0,0 +1,22 @@
+/*
+* Package : iot_voice
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 17/08/2021
+* Copyright :  S.Hamblett
+*/
+
+
+/// Configuration for the IOT Voice GPIO example. Update these as needed.
+
+// Mraa library path.
+const mraaLibraryPath = '/home/ubuntu/Development/mraa/build/src/libmraa.so.2';
+
+// Mraa configuration.
+const noJsonLoading = true;
+const useGrovePi = false;
+
+// The GPIO pin for the Grove LED
+const int ledGPIOPin = 29; // Raspberry PI GPIO pin GPIO5
+
+// The GPIO pin for the Grove momentary switch
+const int switchGPIOPin = 36; //Raspberry PI GPIO pin GPIO16
